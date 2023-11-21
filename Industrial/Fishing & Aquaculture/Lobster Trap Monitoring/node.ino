46f25e43df0b39123e7252136fd587cb8c3fa9a4
Michael Landry <michaellandry3@hotmail.com>
2023-11-21 13:18:43 -0400

    Update node.ino

--- Industrial/Fishing & Aquaculture/Lobster Trap Monitoring/node.ino
+++ Industrial/Fishing & Aquaculture/Lobster Trap Monitoring/node.ino
@@ -1,13 +1,28 @@
 #include <LoRa.h>
 #include <Wire.h>
-#include <SensorLib1.h>  // Replace with the actual library for your sensors
-#include <SensorLib2>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BMP280.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <Wire.h>
+#include <MPU6050.h>
 
 #define LORA_CS 18
 #define LORA_RST 14
 #define LORA_IRQ 26
 
 #define NODE_ID 1  // Unique ID for each node in the mesh network
+
+// Barometric Pressure Sensor (BMP280)
+Adafruit_BMP280 bmp;
+
+// Temperature Sensor (DS18B20)
+#define ONE_WIRE_BUS 4
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+// Accelerometer Gyroscope Sensor (MPU6050)
+MPU6050 mpu;
 
 void setup() {
   Serial.begin(115200);
@@ -20,10 +35,18 @@ void loop() {
   // Read sensor data
   float pressure = readPressureSensor();
   float temperature = readTemperatureSensor();
-  // Additional sensor readings
+  float accelX, accelY, accelZ, gyroX, gyroY, gyroZ;
+  readAccelerometerGyroscope(accelX, accelY, accelZ, gyroX, gyroY, gyroZ);
 
   // Create a message payload
-  String payload = "ID:" + String(NODE_ID) + ",Pressure:" + String(pressure) + ",Temperature:" + String(temperature);
+  String payload = "ID:" + String(NODE_ID) + ",Pressure:" + String(pressure) +
+                   ",Temperature:" + String(temperature) +
+                   ",AccelX:" + String(accelX) +
+                   ",AccelY:" + String(accelY) +
+                   ",AccelZ:" + String(accelZ) +
+                   ",GyroX:" + String(gyroX) +
+                   ",GyroY:" + String(gyroY) +
+                   ",GyroZ:" + String(gyroZ);
   // Additional payload data
 
   // Send data via LoRa
@@ -51,15 +74,32 @@ void sendMessage(String payload) {
 }
 
 void setupSensors() {
-  // Initialize and configure your sensors
+  // Initialize and configure sensors
+  if (!bmp.begin()) {
+    Serial.println("Could not find a valid BMP280 sensor, check wiring!");
+    while (1);
+  }
+
+  sensors.begin();
+
+  if (!mpu.begin()) {
+    Serial.println("Failed to initialize MPU6050 sensor!");
+    while (1);
+  }
 }
 
 float readPressureSensor() {
-  // Read and return pressure sensor data
+  return bmp.readPressure() / 100.0;  // Convert Pa to kPa
 }
 
 float readTemperatureSensor() {
-  // Read and return temperature sensor data
+  sensors.requestTemperatures();
+  return sensors.getTempCByIndex(0);
+}
+
+void readAccelerometerGyroscope(float &accelX, float &accelY, float &accelZ, float &gyroX, float &gyroY, float &gyroZ) {
+  mpu.getAcceleration(&accelX, &accelY, &accelZ);
+  mpu.getRotation(&gyroX, &gyroY, &gyroZ);
 }
 
 // Additional functions for handling mesh networking
